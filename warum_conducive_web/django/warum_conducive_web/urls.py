f12ee6ffbeb40c366a1facec75adac46ed73ccaf
Ashish Bhutiani <ashish@function1.com>
2014-08-13 17:11:57 -0400

    1. Rename User Investigation to Summary 2. Fix urls.py 3. Change Navigation

--- warum_conducive_web/django/warum_conducive_web/urls.py
+++ warum_conducive_web/django/warum_conducive_web/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, include, url
 from splunkdj.utility.views import render_template as render
 
 urlpatterns = patterns('',
-    url(r'^home/$', 'warum_conducive_web.views.home', name='home'), 
+    url(r'^summary/$',render('dg_web:summary.html'),name='summary'),
+    url(r'^user_details/$', render('dg_web:user_details.html'), name='user_details'),
+    url(r'^document_details/$', render('dg_web:document_details.html'), name='document_details'),
 )
