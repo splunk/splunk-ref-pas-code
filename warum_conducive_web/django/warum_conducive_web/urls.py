cc93de78e949f5bfc5ec1d04fe97304baa80277b
Ashish Bhutiani <ashish@function1.com>
2014-08-18 17:59:21 -0400

    Installation Issue
    Fixed URLs to point to warum

--- warum_conducive_web/django/warum_conducive_web/urls.py
+++ warum_conducive_web/django/warum_conducive_web/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from splunkdj.utility.views import render_template as render
 
 urlpatterns = patterns('',
-    url(r'^summary/$',render('dg_web:summary.html'),name='summary'),
-    url(r'^user_details/$', render('dg_web:user_details.html'), name='user_details'),
-    url(r'^document_details/$', render('dg_web:document_details.html'), name='document_details'),
+    url(r'^summary/$',render('warum_conducive_web:summary.html'),name='summary'),
+    url(r'^user_details/$', render('warum_conducive_web:user_details.html'), name='user_details'),
+    url(r'^document_details/$', render('warum_conducive_web:document_details.html'), name='document_details'),
 )
