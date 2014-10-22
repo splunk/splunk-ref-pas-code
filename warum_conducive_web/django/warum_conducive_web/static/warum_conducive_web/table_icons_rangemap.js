1379605572d6b5b1702dcc44d30d11f5ad03376f
Ashish Bhutiani <ashish@function1.com>
2014-10-22 01:07:02 -0400

    Integrate Calendar Heatmap into User Details

---
+++ warum_conducive_web/django/warum_conducive_web/static/warum_conducive_web/table_icons_rangemap.js
@@ -0,0 +1,42 @@
+require([
+    'underscore',
+    'jquery',
+    'splunkjs/mvc',
+    'splunkjs/mvc/tableview',
+    'splunkjs/mvc/simplexml/ready!'
+], function(_, $, mvc, TableView) {
+
+    // Translations from rangemap results to CSS class
+    var ICONS = {
+        severe: 'alert-circle',
+        elevated: 'alert',
+        low: 'check-circle'
+    };
+
+    var RangeMapIconRenderer = TableView.BaseCellRenderer.extend({
+        canRender: function(cell) {
+            // Only use the cell renderer for the range field
+            return cell.field === 'range';
+        },
+        render: function($td, cell) {
+            var icon = 'question';
+            // Fetch the icon for the value
+            if (ICONS.hasOwnProperty(cell.value)) {
+                icon = ICONS[cell.value];
+            }
+            // Create the icon element and add it to the table cell
+            $td.addClass('icon').html(_.template('<i class="icon-<%-icon%> <%- range %>" title="<%- range %>"></i>', {
+                icon: icon,
+                range: cell.value
+            }));
+        }
+    });
+
+    mvc.Components.get('activity_log_all').getVisualization(function(tableView){
+        // Register custom cell renderer
+        tableView.table.addCellRenderer(new RangeMapIconRenderer());
+        // Force the table to re-render
+        tableView.table.render();
+    });
+
+});
