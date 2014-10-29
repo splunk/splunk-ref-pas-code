5c358d95f03ef3f962d1be5aa2d7740b1d2ecbec
David Foster <dfoster@splunk.com>
2014-10-29 13:57:11 -0700

    Delete the Document Details view.

--- warum_conducive_web/django/warum_conducive_web/urls.py
+++ warum_conducive_web/django/warum_conducive_web/urls.py
@@ -4,6 +4,5 @@ from splunkdj.utility.views import render_template as render
 urlpatterns = patterns('',
     url(r'^summary/$','warum_conducive_web.views.summary',name='summary'),
     url(r'^user_details/$', render('warum_conducive_web:user_details.html'), name='user_details'),
-    url(r'^document_details/$', render('warum_conducive_web:document_details.html'), name='document_details'),
     url(r'^setup/$', 'warum_conducive_web.views.setup', name='setup')
 )
