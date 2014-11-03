fadf294a869cafdd91606d72531880c1a9d108a7
Grigori Melnik <gmelnik@splunk.com>
2014-11-03 09:39:43 -0800

    Updated to the final version of the kvstore_backbone lib

--- lib/kvstore_backbone/django/kvstore_backbone/__init__.py
+++
@@ -1,1 +0,0 @@
-# Copyright [TODO: This year]
--- lib/kvstore_backbone/django/kvstore_backbone/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-from splunkdj.utility.views import render_template as render
-
-urlpatterns = patterns('',
-    url(r'^home/$', 'kvstore_backbone.views.home', name='home')
-)
