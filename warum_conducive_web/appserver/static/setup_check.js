b9b52c09df0ef87b638411bdd290d1a03144bd9e
David Foster <dfoster@splunk.com>
2014-11-11 13:42:28 -0800

    Setup Check: Flatten require() usage.

--- warum_conducive_web/appserver/static/setup_check.js
+++ warum_conducive_web/appserver/static/setup_check.js
@@ -4,27 +4,26 @@ require.config({
     }
 });
 
-// Make Splunk ready
-require(['splunkjs/mvc/simplexml/ready!'], function() {
-    require(
-    [
-        'kvstore'
-    ], function() {
-        var KVStore = require('kvstore');
-
-        var RiSetupModel = KVStore.Model.extend({
-            collectionName: 'ri_setup_coll'
-        });
-
-        var model = new RiSetupModel();
-        model.fetch()
-            .done(function(data, textStatus, jqXHR) {
-                if (data.length == 0)
-                {
-                    //Setup hasn't been done yet.
-                    alert("You need to perform setup before coming to this page.");
-                    window.location.href = "./setup";
-                }
-            });
+// If the app hasn't been setup, redirect to the setup page
+require([
+    'splunkjs/mvc/simplexml/ready!',
+    'kvstore'
+], function(
+    ignored,
+    KVStore
+) {
+    var RiSetupModel = KVStore.Model.extend({
+        collectionName: 'ri_setup_coll'
     });
+
+    var model = new RiSetupModel();
+    model.fetch()
+        .done(function(data, textStatus, jqXHR) {
+            if (data.length == 0)
+            {
+                //Setup hasn't been done yet.
+                alert("You need to perform setup before coming to this page.");
+                window.location.href = "./setup";
+            }
+        });
 });
