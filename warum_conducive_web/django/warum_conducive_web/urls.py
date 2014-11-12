aa0d7fb0f82561e3dd5bcb42f70b49c9f0c406a0
David Foster <dfoster@splunk.com>
2014-11-12 12:06:33 -0800

    Remove remaining Django cruft.

--- warum_conducive_web/django/warum_conducive_web/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-from splunkdj.utility.views import render_template as render
-
-urlpatterns = patterns('',
-    url(r'^setup/$', 'warum_conducive_web.views.setup', name='setup')
-)
