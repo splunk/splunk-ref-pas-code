4f5bb0ae7fd012eb72da221701cb8a24edc95008
David Foster <dfoster@splunk.com>
2014-11-26 14:44:33 -0800

    User Info: Convert tabs to spaces.

--- warum_conducive_web/appserver/static/user_info.js
+++ warum_conducive_web/appserver/static/user_info.js
@@ -13,7 +13,7 @@ require([
         '<div class="user_info_box">' +
         // '    <div class="user_name"><%= user_name %></div>' +
         '    <div class="user_fullname"><%= user_fullname %></div>' +
-		'    <div class="user_email"><%= user_email %></div>'+
+        '    <div class="user_email"><%= user_email %></div>'+
         '    <div class="user_phone"><%= user_phone %></div>' +
         '    <div class="company_name"><%= company_name %></div>'+
         '    <div class="user_role"><%= user_role %></div>'+
@@ -23,37 +23,37 @@ require([
         '</div>');
     
     var view = $("#user_info");
-	view.html("Getting user info...");
+    view.html("Getting user info...");
     var userInfoSearch = mvc.Components.get("user_info_search");
-	
-	userInfoSearch.data("results").on("data", function(resultsModel) {
-		var rows = resultsModel.data().rows;
-		if (rows.length === 0) {
-			 view.html("No user information found.");
-		 } else {
-			view.html("");
-			 _.each(rows, function(row) {
-				 var companyAddress = row[0];
-				 var companyName = row[1];
-				 var companyPhone = row[2];
-				 var userEmail = row[3];
-				 var userFullName = row[4];
-				 var userImage = row[5];
-				 // var userName = row[6];
-				 var userPhone = row[7];
-				 var userRole = row[8];
-				 view.append($(USER_INFO_BOX_TEMPLATE({
-					// user_name: userName,
-					user_fullname: userFullName,
-					user_email: userEmail,
-					user_phone: userPhone,
-					company_name: companyName,
-					user_role: userRole,
-					company_address: companyAddress,
-					company_phone: companyPhone,
-					user_image: userImage
-				})));
-			});
-		}
-	});
+    
+    userInfoSearch.data("results").on("data", function(resultsModel) {
+        var rows = resultsModel.data().rows;
+        if (rows.length === 0) {
+            view.html("No user information found.");
+        } else {
+            view.html("");
+            _.each(rows, function(row) {
+                var companyAddress = row[0];
+                var companyName = row[1];
+                var companyPhone = row[2];
+                var userEmail = row[3];
+                var userFullName = row[4];
+                var userImage = row[5];
+                // var userName = row[6];
+                var userPhone = row[7];
+                var userRole = row[8];
+                view.append($(USER_INFO_BOX_TEMPLATE({
+                    // user_name: userName,
+                    user_fullname: userFullName,
+                    user_email: userEmail,
+                    user_phone: userPhone,
+                    company_name: companyName,
+                    user_role: userRole,
+                    company_address: companyAddress,
+                    company_phone: companyPhone,
+                    user_image: userImage
+                })));
+            });
+        }
+    });
  });
