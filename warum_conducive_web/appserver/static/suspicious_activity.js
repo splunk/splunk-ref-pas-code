2b6a16e1fd66ffac380272548729dcb3bad0eb1a
David Foster <dfoster@splunk.com>
2014-12-01 14:38:19 -0800

    Summary: Replace color column in Suspicious Activity panel with color blobs.

---
+++ warum_conducive_web/appserver/static/suspicious_activity.js
@@ -0,0 +1,25 @@
+require([
+    "splunkjs/ready!",
+    "splunkjs/mvc/simplexml/ready!",
+    "splunkjs/mvc/tableview"
+], function(
+    mvc,
+    ignored,
+    TableView
+) {
+    var CustomIconCellRenderer = TableView.BaseCellRenderer.extend({
+        canRender: function(cell) {
+            return cell.field === 'C';  // the color column
+        },
+        
+        render: function($td, cell) {
+            $td.html('<span style="font-size: 3em; color: ' + cell.value + '">&#x25CF;</span>');
+        }
+    });
+    
+    var tableElement = mvc.Components.getInstance('suspicious_activity_table');
+    tableElement.getVisualization(function(tableView) {
+        tableView.table.addCellRenderer(new CustomIconCellRenderer());
+        tableView.table.render();
+    });
+});
