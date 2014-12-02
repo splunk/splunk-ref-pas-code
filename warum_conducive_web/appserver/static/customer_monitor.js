845f9daf7cd1b0dca96a91884daae1d90ec76050
David Foster <dfoster@splunk.com>
2014-12-02 14:11:08 -0800

    Customer Monitor: Instantiate custom view directly instead of relying on shim.

---
+++ warum_conducive_web/appserver/static/customer_monitor.js
@@ -0,0 +1,31 @@
+// Workaround the css! plugin's inability to understand '..' in
+// require'd module names by defining a path that embeds the '..'
+// so that css! doesn't see it.
+require.config({
+    paths: {
+        "warum_conducive_web": "../app/warum_conducive_web"
+    }
+});
+
+require([
+    "splunkjs/ready!",
+    "splunkjs/mvc/simplexml/ready!",
+    "jquery",
+    "warum_conducive_web/components/dendrogram/dendrogram"
+], function(
+    mvc,
+    ignored,
+    $,
+    DendrogramView
+) {
+    new DendrogramView({
+        "managerid": "dendrogram_search",
+        "root_label": mvc.tokenSafe("$customer$"),
+        "right": 600,
+        "height": 600,
+        "initial_open_lavel": 2,
+        "node_outline_color": "#415e70",
+        "node_close_color": "#b9d8eb",
+        "el": $("#dendrogram")
+    }).render();
+});
