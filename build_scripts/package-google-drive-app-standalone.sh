033318bcfe94331c565dfa5f7530387e81cf4cac
Liying Jiang <ljiang@splunk.com>
2015-08-17 14:50:54 -0700

    update package-googledrive-addon build script

--- build_scripts/package-google-drive-app-standalone.sh
+++ build_scripts/package-google-drive-app-standalone.sh
@@ -3,10 +3,10 @@
 pwd/bin/bash
 
 cd splunk-ref-pas-code-ember/pas_ref_app/appserver/addons
-#tar cvzf splunk-googledrive-addon-150.tar.gz googledrive_addon --exclude=./googledrive_addon/default/inputs.conf
+tar cvzf splunk-googledrive-addon-150.tar.gz googledrive_addon --exclude=./googledrive_addon/default/inputs.conf
 
 # For Mac OS X comment the previous tar command and uncomment the following line
- tar cvzf --exclude=googledrive_addon/default/inputs.conf -f splunk-googledrive-addon-150.tar.gz googledrive_addon 
+#tar cvzf --exclude=googledrive_addon/default/inputs.conf -f splunk-googledrive-addon-150.tar.gz googledrive_addon 
 
 mv splunk-googledrive-addon-150.tar.gz ../../../..
 cd ../../../..
