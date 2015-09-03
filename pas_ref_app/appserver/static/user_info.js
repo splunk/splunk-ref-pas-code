2ee997f143a4487e343b0b53ddac95b0b46d8f1b
David Poncelow <dponcelow@splunk.com>
2015-09-03 13:52:07 -0700

    Add user image, fix username select textbox
    Label for username selector was blocking selection of input element.
    Fixed fields for user display (it's based on order and a field had been
                 added)
    Added demo user avatar (requires a change to the test service in the
               test app as well)

--- pas_ref_app/appserver/static/user_info.js
+++ pas_ref_app/appserver/static/user_info.js
@@ -10,8 +10,9 @@ require([
     _
 ) {
     var USER_INFO_BOX_TEMPLATE = _.template(
+    '    <div class="user_image"><img src="<%= user_image %>"/></div>' +
         '<div class="user_info_box">' +
-        // '    <div class="user_name"><%= user_name %></div>' +
+     // '    <div class="user_name"><%= user_name %></div>' +
         '    <div class="user_fullname"><%= user_fullname %></div>' +
         '    <div class="user_email"><%= user_email %></div>'+
         '    <div class="user_phone"><%= user_phone %></div>' +
@@ -19,7 +20,7 @@ require([
         '    <div class="user_role"><%= user_role %></div>'+
         '    <div class="company_address"><%= company_address %></div>' +
         '    <div class="company_phone"><%= company_phone %></div>' +
-        '    <div class="user_image"><%= user_image %></div>' +
+        '    <div class="clearfix"></div>' +
         '</div>');
     
     var view = $("#user_info");
@@ -39,15 +40,16 @@ require([
         } else {
             view.html("");
             _.each(rows, function(row) {
-                var companyAddress = row[0];
-                var companyName = row[1];
-                var companyPhone = row[2];
-                var userEmail = row[3];
-                var userFullName = row[4];
-                var userImage = row[5];
-                // var userName = row[6];
-                var userPhone = row[7];
-                var userRole = row[8];
+                var accountStatus = row[0];
+                var companyAddress = row[1];
+                var companyName = row[2];
+                var companyPhone = row[3];
+                var userEmail = row[4];
+                var userFullName = row[5];
+                var userImage = row[6];
+                var userName = row[7];
+                var userPhone = row[8];
+                var userRole = row[9];
                 view.append($(USER_INFO_BOX_TEMPLATE({
                     // user_name: userName,
                     user_fullname: userFullName,
